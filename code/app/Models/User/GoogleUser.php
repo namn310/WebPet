c188a4b0d71a12c77a623966179779817e3b86d9
namn310 <namn3102003@gmail.com>
2024-11-09 21:54:35 +0700

    Nam push code

---
+++ code/app/Models/User/GoogleUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GoogleUser extends Model
+{
+    use HasFactory;
+}
