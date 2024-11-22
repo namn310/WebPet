910748aa9a17b1ba70a0a195f2ee6503c2d9a973
ninh duy nhật <ninhnhat1811@gmail.com>
2024-11-22 18:53:41 +0700

    update booking views,services view

--- code/app/Http/Controllers/OrderController.php
+++ code/app/Http/Controllers/OrderController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\product;
 use Illuminate\Http\Request;
+use App\Models\User\Order;
+use App\Models\User\OrderDetail;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -11,7 +15,35 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $Order = Order::all();
+        return view('Admin.Quanlydonhang', ['Order' => $Order]);
+    }
+    public function detail($id)
+    {
+        $product = new Order();
+        $totalPrice = $product->getTotalCost($id);
+        $OrderDetail = OrderDetail::select()->where('idOrder', $id)->get();
+        $Order = Order::select()->where('id', $id)->get();
+        // $Order = Order::find($id)->get();
+        return view('Admin.ChiTietDonHang', ['Order' => $Order, 'OrderDetail' => $OrderDetail, 'totalPrice' => $totalPrice]);
+    }
+    public function delivery($id)
+    {
+        // $order = DB::table('orders')->update();
+        $order = Order::find($id);
+        $order->status = 1;
+        $product = OrderDetail::select('number', 'idPro')->where('idOrder', $id)->get();
+        foreach ($product as $row) {
+            $productDetail = product::select('count', 'idPro')->where('idPro', $row->idPro)->get();
+            foreach ($productDetail as $pro) {
+                $updatePro = product::find($pro->idPro);
+                $updatePro->count = $pro->count - $row->number;
+                // dd($pro->count - $row->number);
+                $updatePro->update();
+            }
+        }
+        $order->update();
+        return redirect(route('admin.order'))->with('status', 'Giao hàng thành công');
     }
 
     /**
@@ -59,6 +91,8 @@ class OrderController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $order = Order::find($id);
+        $order->delete();
+        return redirect(route('admin.order'))->with('status', 'Xóa đơn hàng thành công');
     }
 }
